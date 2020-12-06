eec559cf430612883529be6e8bea95bb9f00ece3
Anik7303 <anik7303@gmail.com>
2020-12-06 21:20:31 +0600

    initial project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <header></header>
+            <main className="main">
+                <h1 className="heading-1">Welcome to React</h1>
+            </main>
+            <footer></footer>
+        </div>
+    );
+}
+
+export default App;
